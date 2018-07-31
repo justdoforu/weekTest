cb4072e31981373ec5b14109873a81ab2757927c
justdoforu <498119916@qq.com>
2018-07-31 17:27:20 +0800

    Add files via upload

---
+++ praiseThumb1/index.spec.js
@@ -0,0 +1,5 @@
+describe("基础单元测试",function(){
+    it("点赞+1",function(){
+        expect(window.add(1)).toBe(2);
+    })
+})
---
+++ praiseThumb1/scripts/thumb.es6
@@ -0,0 +1,4 @@
+import Thumb from './index.es6';
+
+let f = new Thumb(0,$('thumb'));
+f.clickAction();
